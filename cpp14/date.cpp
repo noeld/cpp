8a5900045f3fcdb1e9227171338473a07feb4646
Arnold Müller <noeld.mueller@googlemail.com>
2016-01-26 21:16:27 +0100

    20160126

---
+++ cpp14/date.cpp
@@ -0,0 +1,161 @@
+//# /Users/arnold/Documents/prg/clang/bin/clang++ -U__STRICT_ANSI__ -std=c++14 -I/Users/arnold/Documents/prg/libraries/boost_1_56_0/ -I/Developer/SDKs/MacOSX10.6.sdk/usr/include -I/Users/arnold/Documents/prg/libraries/cryptopp563/include -Wall -O2 -L/Users/arnold/Documents/prg/libraries/boost_1_56_0/stage/lib -L/Users/arnold/Documents/prg/libraries/cryptopp563/lib -o date date.cpp -lboost_system -lboost_filesystem -lboost_iostreams -lcryptopp
+
+#include <iostream>
+#include <boost/filesystem.hpp>
+#include <vector>
+#include <string>
+#include <array>
+#include <boost/format.hpp>
+
+using namespace std;
+
+class Date
+{
+public:
+	explicit Date (unsigned yyyy, unsigned mm, unsigned dd) : val_{yyyy, mm, dd}
+	{
+		if (1 > yyyy || 9999 < yyyy)
+			throw "year out of range";
+		if (1 > mm || 12 < mm)
+			throw "month out of range";
+		if (1 > dd || daysOfMonth(yyyy, mm) < dd)
+			throw "days ot of range";
+	}	// type conversion
+	explicit Date (unsigned yyyymmdd) : Date(yyyymmdd/10000, (yyyymmdd/100)%100, yyyymmdd%100) 
+	{}
+	Date() =default;									// default ctor
+	Date(const Date&);							// copy ctor
+	Date(Date&&);								// move ctor
+	Date& operator=(const Date&);				// copy assignment
+	Date& operator=(Date&&);					// move assignment
+	~Date ()
+	{}
+	
+	unsigned year() const noexcept { return val_.yyyy; }
+	unsigned month() const noexcept { return val_.mm; }
+	unsigned day() const noexcept { return val_.dd; }
+	unsigned yyyymmdd() const noexcept { return year()*1000 + month() * 100 + day(); }
+	
+	Date firstOfYear() const noexcept { return Date(year(), 1, 1); }
+	Date lastOfYear() const noexcept { return Date(year(), 12, 31); }
+	Date firstOfMonth() const noexcept { return Date(year(), month(), 1); }
+	Date lastOfMonth() const noexcept { return Date(year(), month(), Date::daysOfMonth(year(), month())); }
+	
+	bool isLeapYear() const noexcept { return Date::isLeapYear(year()); }
+	unsigned dayOfYear() const noexcept {
+		auto d = Date::doma_[month()-1] + day();
+		if (month() > 2 && isLeapYear())
+			++d;
+		return d;
+	}
+	unsigned weekday() const noexcept {
+		// http://www.timeanddate.com/date/doomsday-weekday.html
+		int a1 = (year() % 100)/12;
+		int a2 = (year() % 100) - (12 * a1);
+		int a3 = a2/4;
+		int a4 = Date::anchor_[(year()/100)%4];
+		int a5 = a1 + a2 + a3 + a4;
+		int a6 = a5 % 7;
+		auto zz = a6 + static_cast<int>(dayOfYear()) - 31 - static_cast<int>(Date::daysOfMonth(year(), 2));
+		auto z = (zz) %7;
+		if (zz < 0)
+			z = (7 + z)%7;
+		//cout << "(" << zz << ", " << z << ")";
+		return z;
+	}
+	
+	
+	static bool isLeapYear(unsigned yyyy) noexcept {
+		return (yyyy % 4 == 0) && ((yyyy % 100 != 0) || (yyyy % 400 == 0)); 
+	}
+	static unsigned daysOfYear(unsigned yyyy) noexcept {
+		return isLeapYear(yyyy)?366:365;
+	}
+	static unsigned daysOfMonth(unsigned yyyy, unsigned mm) {
+		auto d = dom_[mm];
+		if (2 == mm && isLeapYear(yyyy)) {
+			d += 1;
+		}
+		return d;
+	}
+	static const char* dayName(unsigned d) {
+		if (d > 6)
+			throw "Weekday out of range";
+		return days_[d];
+	}
+	Date& operator++() {
+		auto d = day() + 1;
+		if (d > Date::daysOfMonth(year(), month())) {
+			++val_.mm;
+			val_.dd = 1;
+			if (13 == val_.mm) {
+				++val_.yyyy;
+				if (9999 < val_.yyyy)
+					throw "date is out of range";
+				val_.mm = 1;
+			}
+		} else {
+			val_.dd = d;
+		}
+		return *this;
+	}
+	Date& operator--() {
+		auto d = day() - 1;
+		if (0 == d) {
+			auto m = month() - 1;
+			if (0 == m) {
+				m = 12;
+				auto y = year() - 1;
+				if (0 == y)
+					throw "year is out of range";
+				val_.yyyy = y;
+			}
+			val_.mm = m;
+			d = Date::daysOfMonth(year(), month());
+		}
+		val_.dd = d;
+		return *this;
+	}
+private:
+	static const std::array<unsigned, 13> dom_;
+	static const std::array<unsigned, 12> doma_;
+	static const std::array<unsigned, 4> anchor_;
+	static const std::array<const char*, 7> days_;
+	struct ct {
+		unsigned yyyy : 14;
+		unsigned mm   :  4;
+		unsigned dd   :  5;
+	} val_ {1,1,1};
+};
+
+constexpr const std::array<unsigned, 13> Date::dom_  = {{ 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }};
+constexpr const std::array<unsigned, 12> Date::doma_ = {{ 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334 }};
+constexpr const std::array<unsigned, 4> Date::anchor_ = {{ 2, 6, 5, 3 }};
+constexpr const std::array<const char*, 7> Date::days_ = {{ "So", "Mo", "Di", "Mi", "Do", "Fr", "Sa" }};
+
+ostream& operator<<(ostream& o, const Date& d) {
+	return o << boost::format{"%04d-%02d-%02d"} % d.year() % d.month() % d.day();
+}
+
+int main (int argc, char const *argv[])
+{
+	int ret {0};
+	cout << sizeof(Date) << endl;
+	Date d;
+	cout << d << endl;
+	Date d2(9999,12,31); cout << d2 << endl;
+	Date d3(20161231); cout << d3 << endl;
+	/*
+	for (unsigned y = 1999; y <= 2020; ++y) {
+			cout << "Date::isLeapYear(" << y << "): " << boolalpha << Date::isLeapYear(y) << endl;
+	}
+	*/
+	/*
+	for(auto i = 1, n = 0; i < 13; ++i, n += Date::dom_[i-1] )
+		cout << i << ": " << n << endl;
+	*/
+	for(Date d(2015,8,31); d.year() > 2013; --d) {
+		cout << d << " - " << boost::format{"%3d - %d - %s"} % d.dayOfYear() % d.weekday() % Date::dayName(d.weekday())<< endl;
+	}
+	return ret;
+}
--- cpp14/mmfile.cpp
+++ cpp14/mmfile.cpp
@@ -1,14 +1,72 @@
-//# /Users/arnold/Documents/prg/clang/bin/clang++ -U__STRICT_ANSI__ -std=c++14 -I/Users/arnold/Documents/prg/libraries/boost_1_56_0/ -I/Developer/SDKs/MacOSX10.6.sdk/usr/include -I/Users/arnold/Documents/prg/libraries/cryptopp563/include -Wall -O2 -L/Users/arnold/Documents/prg/libraries/boost_1_56_0/stage/lib -L/Users/arnold/Documents/prg/libraries/cryptopp563/lib -o mmfile mmfile.cpp -lboost_system -lboost_filesystem -lcryptopp
+//# /Users/arnold/Documents/prg/clang/bin/clang++ -U__STRICT_ANSI__ -std=c++14 -I/Users/arnold/Documents/prg/libraries/boost_1_56_0/ -I/Developer/SDKs/MacOSX10.6.sdk/usr/include -I/Users/arnold/Documents/prg/libraries/cryptopp563/include -Wall -O2 -L/Users/arnold/Documents/prg/libraries/boost_1_56_0/stage/lib -L/Users/arnold/Documents/prg/libraries/cryptopp563/lib -o mmfile mmfile.cpp -lboost_system -lboost_filesystem -lboost_iostreams -lcryptopp
 
 #include <iostream>
 #include <boost/filesystem.hpp>
 #include <vector>
 #include <string>
+#include <boost/iostreams/stream.hpp>
+#include <boost/format.hpp>
 #include <boost/iostreams/device/mapped_file.hpp>
+#include <boost/program_options.hpp>
+
+namespace b = boost;
+namespace bfs = boost::filesystem;
+namespace bpo = boost::program_options;
+
+struct BaseTask {
+	virtual const std::string& Name() const noexcept;
+	virtual void Run(const std::vector<std::string>&)=0;
+	virtual BaseTask& operator()(const std::vector<std::string>& p) final {
+		Run(p);
+		return *this;
+	}
+};
+
+template<typename T>
+struct Range {
+	Range(T start, T end) : start_{start}, end_{end} {}
+	T begin() const { return start_; }
+	T end() const { return end_; }
+private:
+	mutable T start_;
+	mutable T end_;
+};
 
 int main (int argc, char const *argv[])
 {
+	using namespace std;
 	int ret{0};
+	std::string infilename;
+	try {
+		bpo::options_description od{"Optionen"};
+		od.add_options() 
+			("help,h", "Help")
+			("file,f", bpo::value<std::string>(&infilename), "Input file name")
+			("chunksize,chunk,c", bpo::value<intmax_t>(), "Chunk size");
+	} catch (const bpo::error& e) {
+		std::cerr << e.what() << std::endl;
+	}
+	std::vector<std::string> params(argv, argv + argc);
+	for (const auto& p : params) {
+		std::cerr << p << std::endl;
+	}
+//	b::iostreams::stream<b::iostreams::mapped_file_source> file;
+	b::iostreams::mapped_file_source src;
+	if (params.size() > 1) {
+		src.open(params[1]);
+		cout << b::format{"%10d Size"} % src.size() << endl;
+		auto a = b::iostreams::mapped_file_source::alignment();
+		auto chunksize = (64 << 20) / a * a;
+		cout << b::format{"%10d Alignment"} % b::iostreams::mapped_file_source::alignment() << endl;
+		cout << b::format{"%10f Chunk size"} % (chunksize/1024.0/1024.0) << endl;
+		size_t lines {0};
+		//for(const char *it = src.data(), *end = src.data() + src.size(); it != end; ++it) {
+		for(auto& it : Range<const char*>(src.data(), src.data() + src.size())) {	
+			if (it == '\n')
+				++lines;
+		}
+		cout << b::format{"%10d lines"} % lines << endl;
+	}
 	
 	return ret;
 }
