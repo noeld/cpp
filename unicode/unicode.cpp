925b15a1ac466f6dc65c764c5ba1b7e0c4438fe2
Arnold Müller <noeld.mueller@googlemail.com>
2018-01-01 08:18:46 +0100

    20180101

--- unicode/unicode.cpp
+++ unicode/unicode.cpp
@@ -7,6 +7,7 @@
 #include <cmath>
 #include <array>
 #include <bitset>
+#include <memory>
 
 using namespace std;
 
@@ -32,29 +33,43 @@ public:
 	static constexpr const unsigned char MASK_FB4   = 0x07;
 	static constexpr const unsigned char MASK_CB    = 0x3f;
 	
+	// TODO add o_iterator_end
 	template<typename T = ostream_iterator<char>>
-	static void Encode(wchar_t codepoint, T& o_iterator) {
+	static size_t Encode(wchar_t codepoint, T& o_iterator, const T& o_end) {
+        size_t encoded = 0;
+        char buffer[4];
+        size_t l = 1;
 		if (codepoint <= L'\x7F') {
 			// 1 byte
-			*o_iterator = static_cast<char>(codepoint);
+            l = 1;
+			buffer[0] = static_cast<char>(codepoint);
 		} else if (codepoint <= L'\x7FF') {
 			// 2 byte
-			*o_iterator = (PREFIX_FB2) | (codepoint >> 6);
-			*o_iterator = (PREFIX_CB)  | (codepoint & MASK_CB);
+            l = 2;
+            buffer[0] = (PREFIX_FB2) | (codepoint >> 6);
+            buffer[1] = (PREFIX_CB)  | (codepoint & MASK_CB);
 		} else if (codepoint <= L'\xFFFF') {
 			// 3 byte
-			*o_iterator = (PREFIX_FB3) |  (codepoint >> 12);
-			*o_iterator = (PREFIX_CB)  | ((codepoint >> 6) & MASK_CB);
-			*o_iterator = (PREFIX_CB)  |  (codepoint       & MASK_CB);
+            l = 3;
+            buffer[0] = (PREFIX_FB3) |  (codepoint >> 12);
+            buffer[1] = (PREFIX_CB)  | ((codepoint >> 6) & MASK_CB);
+            buffer[2] = (PREFIX_CB)  |  (codepoint       & MASK_CB);
 		} else if (codepoint <= L'\x10FFFF') {
 			// 4 byte
-			*o_iterator = (PREFIX_FB4) |  (codepoint >> 18);
-			*o_iterator = (PREFIX_CB)  | ((codepoint >> 12) & MASK_CB);
-			*o_iterator = (PREFIX_CB)  | ((codepoint >>  6) & MASK_CB);
-			*o_iterator = (PREFIX_CB)  |  (codepoint        & MASK_CB);
+            l = 4;
+            buffer[0] = (PREFIX_FB4) |  (codepoint >> 18);
+            buffer[1] = (PREFIX_CB)  | ((codepoint >> 12) & MASK_CB);
+            buffer[2] = (PREFIX_CB)  | ((codepoint >>  6) & MASK_CB);
+            buffer[3] = (PREFIX_CB)  |  (codepoint        & MASK_CB);
 		} else {
 			throw "Illegal character codepoint";
 		}
+        for (encoded = 0; encoded < l; ++encoded, ++o_iterator) {
+            if (o_iterator == o_end)
+                throw "Output buffer to small";
+            *o_iterator = buffer[encoded];
+        }
+        return encoded;
 	}
 	union ByteAttributes {
 		uint16_t a {0};
@@ -68,7 +83,7 @@ public:
 		};
 	};
 	
-	static ByteAttributes DecomposeByte(char byte) {
+	static ByteAttributes DecomposeByte(char byte) noexcept {
 		// auto t = byte & PMASK_CB;
 		// cout << "*" << static_cast<unsigned char>(byte) << "*";
 		// cout << bitset<8>(byte) << "-" << bitset<8>(PMASK_FB1) << "-" << bitset<8>(byte & PMASK_FB1) << "-"
@@ -82,42 +97,42 @@ public:
 			ba.bitLength          = 6;
 		} 
 		else if ((byte & PMASK_FB1) == PREFIX_FB1){
-			ba.isFirstByte     = 1;
-			ba.codePointLength = 1;
-			ba.isLegalByte     = 1;
-			ba.bits            = byte & MASK_FB1;
-			ba.bitLength       = 7;
+			ba.isFirstByte        = 1;
+			ba.codePointLength    = 1;
+			ba.isLegalByte        = 1;
+			ba.bits               = byte & MASK_FB1;
+			ba.bitLength          = 7;
 		} 
 		else if ((byte & PMASK_FB2) == PREFIX_FB2){
-			ba.isFirstByte     = 1;
-			ba.codePointLength = 2;
-			ba.isLegalByte     = 1;
-			ba.bits            = byte & MASK_FB2;
-			ba.bitLength       = 5;
+			ba.isFirstByte        = 1;
+			ba.codePointLength    = 2;
+			ba.isLegalByte        = 1;
+			ba.bits               = byte & MASK_FB2;
+			ba.bitLength          = 5;
 		} 
 		else if ((byte & PMASK_FB3) == PREFIX_FB3){
-			ba.isFirstByte     = 1;
-			ba.codePointLength = 3;
-			ba.isLegalByte     = 1;
-			ba.bits            = byte & MASK_FB3;
-			ba.bitLength       = 4;
+			ba.isFirstByte        = 1;
+			ba.codePointLength    = 3;
+			ba.isLegalByte        = 1;
+			ba.bits               = byte & MASK_FB3;
+			ba.bitLength          = 4;
 		} 
 		else if ((byte & PMASK_FB4) == PREFIX_FB4){
-			ba.isFirstByte     = 1;
-			ba.codePointLength = 4;
-			ba.isLegalByte     = 1;
-			ba.bits            = byte & MASK_FB4;
-			ba.bitLength       = 3;
+			ba.isFirstByte        = 1;
+			ba.codePointLength    = 4;
+			ba.isLegalByte        = 1;
+			ba.bits               = byte & MASK_FB4;
+			ba.bitLength          = 3;
 		}
 		return ba; // no legal first byte
 	}
-	static bool IsFirstByte(char byte) {
+	static bool IsFirstByte(char byte) noexcept {
 		return DecomposeByte(byte).isFirstByte == 1;
 	}
-	static bool IsContinuationByte(char byte) {
+	static bool IsContinuationByte(char byte) noexcept {
 		return DecomposeByte(byte).isContinuationByte == 1;
 	}
-	static bool IsUTF8Byte(char byte) {
+	static bool IsUTF8Byte(char byte) noexcept {
 		return DecomposeByte(byte).isLegalByte == 1;
 	}
 	#define CHECKCONTINUATION(b) if (!IsContinuationByte(b)) {\
@@ -129,7 +144,7 @@ public:
 		codepoint  = 0;
 		if (i_iterator == i_end)
 			return;
-		for (size_t l = 1, maxl = 1; l <= maxl; ++l) {
+		for (size_t l = 1, maxl = 1; l <= maxl; ++l, ++i_iterator) {
 			ByteAttributes ba = DecomposeByte(*i_iterator);
 			if (!ba.isLegalByte)
 				throw "Illegal octet";
@@ -145,9 +160,40 @@ public:
             cp |= ba.bits;
 			if (i_iterator == i_end)
 				throw "EOF before codepoint completed";
-			++i_iterator;
 		}
 		codepoint = cp;
+	}
+
+	static size_t DecodeBuffer(const char* srcbuffer, size_t srclen, wchar_t* dstbuffer, size_t dstlen) {
+		const char* srcit     = &srcbuffer[0];
+		const char* srcend    = srcbuffer + srclen;
+		wchar_t* dstit        = &dstbuffer[0];
+		const wchar_t* dstend = dstbuffer + dstlen;
+		size_t decoded        = 0;
+		while(srcit != srcend && dstit != dstend) {
+			Decode(srcit, srcend, *dstit);
+			++decoded;
+			++dstit;
+		}
+		if (dstbuffer != dstend) {
+			*dstit = L'\0';
+		}
+		return decoded;
+	}
+	static size_t EncodeBuffer(const wchar_t* srcbuffer, size_t srclen, char* dstbuffer, size_t dstlen) {
+		const wchar_t* srcit  = &srcbuffer[0];
+		const wchar_t* srcend = srcbuffer + srclen;
+		char* dstit           = &dstbuffer[0];
+		char *const &dstend    = dstbuffer + dstlen;
+		size_t encoded        = 0;
+		while(srcit != srcend && dstit != dstend) {
+			encoded += Encode(*srcit, dstit, dstend);
+			++srcit;
+		}
+		if (dstbuffer != dstend) {
+			*dstit = '\0';
+		}
+		return encoded;
 	}
 };
 
@@ -186,6 +232,19 @@ void test_ed() {
 		
 	}
 	wcout << "Ergebnis: " << oss.str() << endl;
+	constexpr const size_t WBUFFERSIZE = 4096;
+	constexpr const size_t CBUFFERSIZE = WBUFFERSIZE * 4;
+	auto buffer = std::make_unique<wchar_t[]>(WBUFFERSIZE);
+	wcout << "****************" << endl;
+	auto decoded = UTF8::DecodeBuffer(in.data(), in.size(), buffer.get(), WBUFFERSIZE);
+	wcout << "Decoded " << decoded << " wide characters" << endl;
+	wcout << "Result: " << buffer.get() << "(" << std::wcslen(buffer.get()) << ")" << endl;
+	auto cbuffer = std::make_unique<char[]>(CBUFFERSIZE);
+	auto encoded = UTF8::EncodeBuffer(buffer.get(), decoded, cbuffer.get(), CBUFFERSIZE);
+	cout << "Encoded " << encoded << " characters" << endl;
+	std::string res(cbuffer.get());
+	cout << "Result: " << res << "(" << res.size() << ")" << endl;
+	cout << "in " << ((res == in)?"==":"!=") << " res" << endl;
 }
 
 int main(int argc, char const *argv[])
