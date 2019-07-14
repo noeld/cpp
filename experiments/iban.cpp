3579e6e59041e341426bd527e9735e0968f7dd67
Arnold Müller <noeld.mueller@googlemail.com>
2019-07-14 07:53:51 +0200

    20190714_2

--- experiments/iban.cpp
+++ experiments/iban.cpp
@@ -19,15 +19,13 @@ std::string convert_lc(const std::string& lc) {
     return conv_digit(lc[0]) + conv_digit(lc[1]);
 }
 
-int main(int argn, char* argv[]) {
-    if (argn == 1) {
-        usage();
-        exit(1);
-    }
-    std::string iban = argv[1];
+int check_iban(const std::string iban) {
+    int ret = 0;
+    std::cout << "=== Checking " << iban << "===" << std::endl;
     const std::regex re ("[A-Z][A-Z]\\d{20}", std::regex::icase);
     if (! std::regex_match(iban, re)) {
         std::cout << "IBAN ist falsch formatiert" << std::endl;
+        ret = 1;
     }
     const std::regex ree("^(..)(..)(.+)$");
     std::smatch m;
@@ -47,5 +45,22 @@ int main(int argn, char* argv[]) {
         std::cout << "98 - (" << i << " % 97) = " << calc_pz_str << std::endl;
         auto res = pz == calc_pz_str;
         std::cout << "Die Prüfziffern stimmen" << ( res ? "." : " nicht!") << std::endl;
+        if (!res)
+            ret = 1;
     }
+    return ret;
+}
+
+// ./iban DE5837040044028465250 DE58370400440284652500
+int main(int argn, char* argv[]) {
+    std::ios_base::sync_with_stdio(false);
+    if (argn == 1) {
+        usage();
+        exit(1);
+    }
+    int ret = 0;
+    for (int i = 1; i < argn; ++i) {
+        ret = std::max(ret, check_iban(argv[i]));
+    }
+    return ret;
 }
