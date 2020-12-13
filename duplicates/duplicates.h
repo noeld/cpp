572a145f8c79f7292b7b0611822ed34792df4e9c
noeld <noeld.mueller@googlemail.com>
2020-12-13 21:38:19 +0100

    Add duplicates

---
+++ duplicates/duplicates.h
@@ -0,0 +1,32 @@
+﻿// duplicates.h: Includedatei für Include-Standardsystemdateien
+// oder projektspezifische Includedateien.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Verweisen Sie hier auf zusätzliche Header, die Ihr Programm erfordert.
+
+/**
+* Find ranges (neighbouring elements) of the same value within [begin, end[ and
+* call callback for each such range
+* @param begin start of container
+* @param end end of container (1 beyond last element)
+* @param function returns value for each iterator V(*T&)
+* @param callback void(start, end, value)
+* @return number of range
+*/
+template<typename  T, typename V, typename F>
+size_t for_each_adjacent_range(T begin, T end, V getvalue, F callback) {
+	size_t partitions = 0;
+	while (begin != end) {
+		auto const& value = getvalue(*begin);
+		auto current = begin;
+		while (++current != end && getvalue(*current) == value);
+		callback(begin, current, value);
+		++partitions;
+		begin = current;
+	}
+	return partitions;
+}
+
---
+++ duplicates/test.cpp
@@ -0,0 +1,18 @@
+﻿// duplicates.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "duplicates.h"
+
+
+void test_for_each_adjacent_range() {
+	auto bla = std::initializer_list<std::pair<int, char>>{ {1, 'A'}, {2, 'A'}, {3, 'B'}, {4, 'C'}, {5, 'C'}, {6, 'C'} };
+	for_each_adjacent_range(std::begin(bla), std::end(bla)
+		, [](std::pair<int, char> const& e) { return e.second; }
+		, [](auto const& start, auto const& end, auto const& v) {
+			std::cout << "Wert: " << v << " " << std::distance(start, end) << " Elemente.\n";
+		});
+}
+
+int main(int argn, char* argv[]) {
+	test_for_each_adjacent_range();
+}
